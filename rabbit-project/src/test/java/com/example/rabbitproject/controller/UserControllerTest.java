37deacc2b7a006bd1902f9fb0c09f26a0b23dc6d
Diego Lopes <diego.lopes@senior.com.br>
2021-06-23 08:40:46 -0300

    Execute tests in build

--- rabbit-project/src/test/java/com/example/rabbitproject/controller/UserControllerTest.java
+++ rabbit-project/src/test/java/com/example/rabbitproject/controller/UserControllerTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations="classpath:application-test.properties")
-public class UserControllerIT {
+public class UserControllerTest {
 
     @LocalServerPort
     private int port;
