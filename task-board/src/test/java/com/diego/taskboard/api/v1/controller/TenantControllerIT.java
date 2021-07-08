aa9fee0ca550808e16bc59e63aa3cf2785e333d9
Diego Lopes <diego.lopes@senior.com.br>
2021-07-08 09:57:57 -0300

    Add mongo testcontainers on integration tests

--- task-board/src/test/java/com/diego/taskboard/api/v1/controller/TenantControllerIT.java
+++ task-board/src/test/java/com/diego/taskboard/api/v1/controller/TenantControllerIT.java
@@ -16,7 +16,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
