a7b9fad3b8b6ca6e171535493c9ae9ca2b9d0ddb
Diego Lopes <diego.lopes@senior.com.br>
2021-11-18 22:55:32 -0300

    Initial commit

---
+++ desafio-inter/src/main/java/com/inter/desafiointer/DesafioInterApplication.java
@@ -0,0 +1,13 @@
+package com.inter.desafiointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInterApplication.class, args);
+	}
+
+}
